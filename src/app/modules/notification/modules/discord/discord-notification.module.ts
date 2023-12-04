6f21e80efaebe1399e6259b44e1eee112e8cd85b
Sergiu <chilatsergiu@gmail.com>
2023-12-04 18:12:16 +0200

    Discord message(DB store)

--- src/app/modules/notification/modules/discord/discord-notification.module.ts
+++ src/app/modules/notification/modules/discord/discord-notification.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { DiscordNotificationController } from './discord-notification.controller';
 import { DiscordNotificationService } from './discord-notification.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {DiscordNotification} from '@/app/modules/notification/modules/discord/entities/discord-notification.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DiscordNotification])],
   controllers: [DiscordNotificationController],
   providers: [DiscordNotificationService],
 })
--- src/app/modules/notification/modules/discord/discord-notification.service.ts
+++ src/app/modules/notification/modules/discord/discord-notification.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import * as Discord from 'discord.js';
 import {
   DiscordNotificationCreatePayloadDto
 } from '@/app/modules/notification/modules/discord/dto/discord-notification-create-payload.dto';
+import {InjectRepository} from '@nestjs/typeorm';
+import {DiscordNotification} from '@/app/modules/notification/modules/discord/entities/discord-notification.entity';
+import {Repository} from 'typeorm';
+import {Language} from '@/app/enum/language.enum';
 
 @Injectable()
 export class DiscordNotificationService {
+  constructor(
+    @InjectRepository(DiscordNotification)
+    private readonly discordNotificationRepository: Repository<DiscordNotification>
+  ) {
+
+  }
 
   async sendNotification(notification: DiscordNotificationCreatePayloadDto) {
     try {
       for (const notificationReceiverChannel of notification.receivers) {
+        const createdNotification = await this.discordNotificationRepository.save({
+          receiver_channel: notificationReceiverChannel,
+          subject: notification.subject,
+          body: notification.body,
+          sender: notification.sender,
+          language: notification.language || Language.EN
+        });
+
         const client = new Discord.WebhookClient({
           url: `https://discord.com/api/webhooks/${notificationReceiverChannel}`,
         });
@@ -24,6 +42,13 @@ export class DiscordNotificationService {
           }],
         });
 
+        if(response.id){
+          await this.discordNotificationRepository.update({
+            id: createdNotification.id
+          }, {
+            sent_at: new Date()
+          });
+        }
         console.log('Sent', response);
       }
     } catch (e) {
